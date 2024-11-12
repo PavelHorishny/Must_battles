ad5440118e3630d753dd5a4d2d98b906de7a9cf0
Pavel Horishny <horishnypavel@gmail.com>
2024-11-12 18:29:35 +0100

    update method added

--- src/main/java/org/game/gui/panels/Mediator.java
+++ src/main/java/org/game/gui/panels/Mediator.java
@@ -1,5 +1,6 @@
 package org.game.gui.panels;
 
+import org.game.State;
 import org.game.gui.panels.game.components.GameComponent;
 
 public interface Mediator {
@@ -8,6 +9,7 @@ public interface Mediator {
     void unitSelected(Message message);
 
 
-    void gameStarted(Message message);
+    void gameStarted();
     void test (String message);
+    void update(State state);
 }

fff316d96860e52316349120867fcb97212c9c26
Pavel Horishny <horishnypavel@gmail.com>
2024-11-12 18:19:34 +0100

    refactor

--- src/main/java/org/game/gui/UnitIcons.java
+++ src/main/java/org/game/gui/UnitIcons.java
@@ -2,5 +2,5 @@ package org.game.gui;
 
 import java.awt.image.BufferedImage;
 
-public record UnitIcons(BufferedImage icon,BufferedImage active,BufferedImage underAttack) {
+public record UnitIcons(BufferedImage icon,BufferedImage active,BufferedImage aimed) {
 }

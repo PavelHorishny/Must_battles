ed1e7c822ad7ae7e0e4336cb060a9fa149bc228d
Pavel Horishny <horishnypavel@gmail.com>
2024-11-12 18:28:45 +0100

    refactor

--- src/main/java/org/game/gui/panels/game/GamePanel.java
+++ src/main/java/org/game/gui/panels/game/GamePanel.java
@@ -1,5 +1,6 @@
 package org.game.gui.panels.game;
 
+import org.game.Context;
 import org.game.gui.panels.*;
 import org.game.gui.panels.game.components.*;
 
@@ -8,25 +9,30 @@ import javax.swing.*;
 public class GamePanel extends JPanel {
     private GamePanelMediator mediator;
     public GamePanel(){
+        mediator = (GamePanelMediator) Context.getMediator();
         init();
+        //mediator.gameStarted();
        }
     private void init(){
         setLayout(PanelsConstrains.LAYOUT);
         GamePanelComponent mapArea = new MapArea(PanelsConstrains.GAME_AREA_SETTINGS);
-        //mediator.registerComponent(mapArea);
+        mediator.registerComponent(mapArea);
         add(mapArea, PanelsConstrains.GAME_AREA);
         GamePanelComponent windRoseArea = new WindRoseArea(PanelsConstrains.WIND_ROSE_AREA_SETTINGS);
-        //mediator.registerComponent(windRoseArea);
+        mediator.registerComponent(windRoseArea);
         add(windRoseArea,PanelsConstrains.WIND_ROSE_AREA);
         GamePanelComponent buttonArea = new ButtonArea(PanelsConstrains.BUTTON_AREA_SETTINGS);
-        //mediator.registerComponent(buttonArea);
+        mediator.registerComponent(buttonArea);
         add(buttonArea,PanelsConstrains.BUTTON_AREA);
         GamePanelComponent infoArea = new InfoArea(PanelsConstrains.INFO_AREA_SETTINGS);
-        //mediator.registerComponent(infoArea);
+        mediator.registerComponent(infoArea);
         add(infoArea,PanelsConstrains.INFO_AREA);
         GamePanelComponent logArea = new LogArea(PanelsConstrains.LOG_AREA_SETTINGS);
+        mediator.registerComponent(logArea);
         add(logArea,PanelsConstrains.LOG_AREA);
+
         //mediator.test("test");
+        //mediator.gameStarted();
     }
 
     public void setMediator(GamePanelMediator mediator) {

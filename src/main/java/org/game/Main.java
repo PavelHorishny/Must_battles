f10598cbacef709dbd4d6075cbe4407c60dbe697
Pavel Horishny <horishnypavel@gmail.com>
2024-11-12 18:29:56 +0100

    start server

--- src/main/java/org/game/Main.java
+++ src/main/java/org/game/Main.java
@@ -2,11 +2,14 @@ package org.game;
 
 
 import org.game.gui.frame.AppFrame;
+import org.game.messaging.GameServer;
 
 
 public class Main {
     public static void main(String[] args)  {
         System.out.println("Hello world!");
+        new GameServer();
         new AppFrame();
+
     }
 }

04be28c6f76f25d587c30855990f8b2204465af9
Pavel Horishny <horishnypavel@gmail.com>
2024-11-12 18:26:25 +0100

    Update state method added

--- src/main/java/org/game/gui/panels/game/components/GameComponent.java
+++ src/main/java/org/game/gui/panels/game/components/GameComponent.java
@@ -1,8 +1,12 @@
 package org.game.gui.panels.game.components;
 
+import org.game.GameComponentState;
+import org.game.State;
 import org.game.gui.panels.Mediator;
 
 public interface GameComponent {
+    
     void setMediator(Mediator mediator);
     String getGameComponentName();
+    void updateState(GameComponentState state);
 }

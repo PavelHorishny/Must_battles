625cf43725663684bb2adae7e0e943be511264a5
Pavel Horishny <horishnypavel@gmail.com>
2024-11-17 22:55:10 +0100

    Map area state updated

--- src/main/java/org/game/MapAreaState.java
+++ src/main/java/org/game/MapAreaState.java
@@ -3,13 +3,14 @@ package org.game;
 import lombok.Getter;
 import lombok.Setter;
 import org.game.gui.MapCell;
-import org.game.gui.panels.game.components.MapArea;
+import org.game.unit.GUIUnit;
 
 import java.util.ArrayList;
 
 @Getter
 @Setter
 public class MapAreaState extends GameComponentState{
+    ArrayList<GUIUnit> fleet = new ArrayList<>();
     public MapAreaState (ArrayList<MapCell> map){
         this.map = map;
     }

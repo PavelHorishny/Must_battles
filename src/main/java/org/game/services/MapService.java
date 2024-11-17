9db0381536d82e5b8233867086e0bf933e814000
Pavel Horishny <horishnypavel@gmail.com>
2024-11-17 22:49:27 +0100

    Map service interface updated

--- src/main/java/org/game/services/MapService.java
+++ src/main/java/org/game/services/MapService.java
@@ -1,8 +1,12 @@
 package org.game.services;
 
+import org.game.State;
 import org.game.map.Surface;
+import org.game.unit.Fortification;
 
 public interface MapService {
-    Surface [] [] getStaticMap();
+    Surface [] [] generateStandardMap();
+    void setPortLocations(Surface [][] map, Fortification fortification);
     void getRandomMap();
+
 }

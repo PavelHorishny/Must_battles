12bc16af5fc4dbdb202797cf925aeb870e8df06f
Pavel Horishny <horishnypavel@gmail.com>
2024-11-17 22:48:44 +0100

    State class updated and method update implemented

--- src/main/java/org/game/State.java
+++ src/main/java/org/game/State.java
@@ -1,11 +1,25 @@
 package org.game;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.game.map.Surface;
+import org.game.unit.GameUnit;
+
+import java.util.Map;
 
 @Getter
 @Setter
+@Builder
 public class State {
+    Surface [] [] map;
+    Map <String, GameUnit> fleet;
     MapAreaState mapAreaState;
     LogAreaState logAreaState;
+    public State update(){
+        mapAreaState = new MapAreaState();
+        mapAreaState.setMap(BackToGUIConverter.convertMap(map));
+        //mapAreaState.setFleet();
+        return this;
+    }
 }

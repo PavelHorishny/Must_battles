96e5b2a1e03966f831553b362dd80f6e3772d020
Pavel Horishny <horishnypavel@gmail.com>
2024-11-17 22:48:02 +0100

    surface refactored updated

--- src/main/java/org/game/map/Surface.java
+++ src/main/java/org/game/map/Surface.java
@@ -8,21 +8,21 @@ import org.game.unit.GameUnit;
 @Getter
 @Setter
 public class Surface {
-    private int X;
-    private int Y;
+/*    private int X;
+    private int Y;*/
     private Coordinates coordinates;
     private SurfaceType type;
     private GameUnit unit;
     private boolean isUnderAttack;
 
-    public Surface(int x, int y, SurfaceType type) {
+/*    public Surface(int x, int y, SurfaceType type) {
         X = x;
         Y = y;
         this.type = type;
     }
     public Surface (Coordinates coordinates){
         this.coordinates=coordinates;
-    }
+    }*/
     public Surface (Coordinates coordinates,SurfaceType type){
         this.coordinates=coordinates;
         this.type = type;

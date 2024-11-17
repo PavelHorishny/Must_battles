94db05186bb1981f07bed445a96bd68b2fc162bd
Pavel Horishny <horishnypavel@gmail.com>
2024-11-17 22:52:01 +0100

    Map processor class implemented methods of checking if coordinates are valid and if surface object is Water type

--- src/main/java/org/game/services/MapProcessor.java
+++ src/main/java/org/game/services/MapProcessor.java
@@ -1,5 +1,6 @@
 package org.game.services;
 
+import org.game.CardinalPoint;
 import org.game.gui.Constants;
 import org.game.gui.Coordinates;
 import org.game.gui.panels.PanelsConstrains;
@@ -7,13 +8,15 @@ import org.game.map.Surface;
 import org.game.map.SurfaceType;
 
 import org.game.mockData.MockedData;
+import org.game.unit.Fortification;
 
 import java.util.Arrays;
 
 public class MapProcessor implements MapService{
 
+
     @Override
-    public Surface[][] getStaticMap() {
+    public Surface[][] generateStandardMap() {
         Surface [] [] tmp = new Surface[PanelsConstrains.GAME_AREA_SETTINGS.getPreferredSize().width/ Constants.CELL_SIZE][PanelsConstrains.GAME_AREA_SETTINGS.getPreferredSize().height/Constants.CELL_SIZE];
         for (int i = 0; i < tmp.length; i++){
             for (int y = 0; y < tmp[0].length; y++){
@@ -26,7 +29,42 @@ public class MapProcessor implements MapService{
     }
 
     @Override
+    public void setPortLocations(Surface[][] map, Fortification fortification) {
+
+        Arrays.stream(CardinalPoint.cardinalPoints).forEach(cardinalPoint -> {
+           if (checkIfPositionValid(map,new Coordinates(fortification.getCoordinates().axisX()+cardinalPoint.getValue().axisX(),fortification.getCoordinates().axisY()+cardinalPoint.getValue().axisY()))){
+               map[fortification.getCoordinates().axisX()+cardinalPoint.getValue().axisX()][fortification.getCoordinates().axisY()+cardinalPoint.getValue().axisY()].setType(SurfaceType.PORT);
+               fortification.getPort().add(map[fortification.getCoordinates().axisX()+cardinalPoint.getValue().axisX()][fortification.getCoordinates().axisY()+cardinalPoint.getValue().axisY()]);
+           }
+        });
+    }
+
+    @Override
     public void getRandomMap() {
 
+    }
+
+
+    private boolean checkIfPositionValid(Surface [][] map, Coordinates coordinates){
+        //Surface surface = map[coordinates.axisX()][coordinates.axisY()];
+        if(checkValidPositionOnAxisX(map,coordinates)){
+            if(checkValidPositionOnAxisY(map,coordinates)){
+                return checkIfSurfaceIsWater(map[coordinates.axisX()][coordinates.axisY()]);
+            }else {
+                return false;
+            }
+        }else {
+            return false;
+        }
+        //return surface.getCoordinates().axisX() >= 0 && surface.getCoordinates().axisX() < map.length;
+    }
+    private boolean checkValidPositionOnAxisX(Surface [][] map, Coordinates coordinates){
+        return coordinates.axisX() >= 0 && coordinates.axisX() < map.length;
+    }
+    private boolean checkValidPositionOnAxisY(Surface [][] map, Coordinates coordinates){
+        return coordinates.axisY() >= 0 && coordinates.axisY() < map[0].length;
+    }
+    private boolean checkIfSurfaceIsWater(Surface surface){
+        return surface.getType() == SurfaceType.WATER;
     }
 }

af9f131e27ab50ed0683f3ff6b2eaae43a406f9a
Pavel Horishny <horishnypavel@gmail.com>
2024-11-17 22:57:12 +0100

    Fortification interface updated

--- src/main/java/org/game/services/FortificationService.java
+++ src/main/java/org/game/services/FortificationService.java
@@ -1,11 +1,10 @@
 package org.game.services;
 
-import org.game.unit.Fortification;
 import org.game.unit.GameUnit;
 
-import java.util.List;
 import java.util.Map;
 
 public interface FortificationService {
-    public void setNames(Map<String, GameUnit> fleet, List<Fortification> fortifications);
+    //public void setNames(Map<String, GameUnit> fleet, List<Fortification> fortifications);
+    public void generateFortification(Map<String,GameUnit> fleet);
 }

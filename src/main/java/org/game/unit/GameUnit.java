8556b401078371bd6751cd35b77ec8636bfe3345
Pavel Horishny <horishnypavel@gmail.com>
2024-11-17 15:51:55 +0100

    update GameUnit class

--- src/main/java/org/game/unit/GameUnit.java
+++ src/main/java/org/game/unit/GameUnit.java
@@ -2,15 +2,22 @@ package org.game.unit;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.game.gui.Coordinates;
 
 @Setter
 @Getter
 public class GameUnit extends Unit {
-    private UnitType unitType;
+    //private UnitType unitType;
     private boolean isFirstPlayer;
     private String description;
     private int fire_range;
     private int hit_points;
     private int shots;
     private int movePoints;
+
+    public GameUnit(boolean isFirstPlayer, Coordinates coordinates) {
+        //this.unitType = unitType;
+        this.isFirstPlayer = isFirstPlayer;
+        setCoordinates(coordinates);
+    }
 }

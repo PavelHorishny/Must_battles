9dbddc23d273876b1091a25b83c29bdbd669b9fb
Pavel Horishny <horishnypavel@gmail.com>
2024-11-17 15:52:36 +0100

    update Vessel class

--- src/main/java/org/game/unit/Vessel.java
+++ src/main/java/org/game/unit/Vessel.java
@@ -1,8 +1,14 @@
 package org.game.unit;
 
+import org.game.gui.Coordinates;
+
 public class Vessel extends GameUnit {
     private VesselType type;
     private int breeze_move_points;
     private int calm_move_points;
     private int storm_move_points;
+
+    public Vessel(boolean isFirstPlayer, Coordinates coordinates) {
+        super(isFirstPlayer, coordinates);
+    }
 }

45ec8b7b445fe2e13dee22f781574b51df393e92
Pavel Horishny <horishnypavel@gmail.com>
2024-11-17 22:58:44 +0100

    Fortification class updated

--- src/main/java/org/game/unit/Fortification.java
+++ src/main/java/org/game/unit/Fortification.java
@@ -1,14 +1,22 @@
 package org.game.unit;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.game.gui.Coordinates;
+import org.game.map.Surface;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
+@Setter
 public class Fortification extends GameUnit {
     private final FortificationType fortificationType;
+    private List<Surface> port = new ArrayList<>();
 
     public Fortification(Coordinates coordinates,FortificationType fortificationType,boolean player){
         super(player,coordinates);
         this.fortificationType = fortificationType;
+        this.setUnitType(UnitType.FORTIFICATION);
     }
 }

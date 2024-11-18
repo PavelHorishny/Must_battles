0389a3fe0c4cf0109240d31c55245bdcd0907e17
Pavel Horishny <horishnypavel@gmail.com>
2024-11-18 20:26:01 +0100

    vessel class updated

--- src/main/java/org/game/unit/Vessel.java
+++ src/main/java/org/game/unit/Vessel.java
@@ -1,9 +1,10 @@
 package org.game.unit;
 
+import lombok.Getter;
 import org.game.gui.Coordinates;
-
+@Getter
 public class Vessel extends GameUnit {
-    private VesselType type;
+    private VesselType vesselType;
     private int breeze_move_points;
     private int calm_move_points;
     private int storm_move_points;

d77bca03416b93edecddfb132d00e63c15f143f0
Pavel Horishny <horishnypavel@gmail.com>
2024-11-18 20:24:43 +0100

    unit and gui unit updated

--- src/main/java/org/game/unit/GUIUnit.java
+++ src/main/java/org/game/unit/GUIUnit.java
@@ -1,5 +1,6 @@
 package org.game.unit;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.game.gui.Coordinates;
@@ -8,14 +9,20 @@ import org.game.gui.StateType;
 import java.awt.image.BufferedImage;
 @Getter
 @Setter
+@Builder
 public class GUIUnit extends Unit {
+
+
     private UnitIcons icons;
     private BufferedImage currentIcon;
     private StateType currentState;
-    /*int x;
-    int y;
-    Coordinates coordinates;*/
-    
+
+    public GUIUnit() {
+    }
+    public GUIUnit(UnitType type){
+
+    }
+
     public GUIUnit(UnitIcons icons, Coordinates coordinates, StateType state){
         this.icons = icons;
         //this.coordinates=coordinates;
@@ -34,5 +41,29 @@ public class GUIUnit extends Unit {
 /*    private void setPosition(Coordinates coordinates){
         x=coordinates.axisX()*Constants.CELL_SIZE;
         y=coordinates.axisY()*Constants.CELL_SIZE;
+    }*//*
+    @Override
+    public Coordinates getCoordinates() {
+    return super.getCoordinates();
+    }
+
+    @Override
+    public String getId() {
+        return super.getId();
+    }
+
+    @Override
+    public void setCoordinates(Coordinates coordinates) {
+        super.setCoordinates(coordinates);
+    }
+
+    @Override
+    public void setId(String id) {
+        super.setId(id);
+    }*/
+/*    public void setCurrentIcon(UnitIcons icons){
+        if(icons==null){
+            currentIcon =
+        }
     }*/
 }
--- src/main/java/org/game/unit/GameUnit.java
+++ src/main/java/org/game/unit/GameUnit.java
@@ -3,11 +3,13 @@ package org.game.unit;
 import lombok.Getter;
 import lombok.Setter;
 import org.game.gui.Coordinates;
+import org.game.gui.StateType;
 
 @Setter
 @Getter
 public class GameUnit extends Unit {
     private UnitType unitType;
+    private StateType stateType;
     private boolean isFirstPlayer;
     private String description;
     private int fire_range;

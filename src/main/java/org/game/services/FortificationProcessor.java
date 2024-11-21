0a7e825b024a774a29cb1fa2187330ca52c55dd2
Pavel Horishny <horishnypavel@gmail.com>
2024-11-21 01:10:21 +0100

    fortification processor standard fortification method implemented

--- src/main/java/org/game/services/FortificationProcessor.java
+++ src/main/java/org/game/services/FortificationProcessor.java
@@ -1,21 +1,77 @@
 package org.game.services;
 
+import org.game.CardinalPoint;
+import org.game.gui.Coordinates;
+import org.game.map.Surface;
+import org.game.map.SurfaceType;
 import org.game.mockData.MockedData;
 import org.game.mockData.NamesRandomizer;
 import org.game.unit.Fortification;
 import org.game.unit.FortificationType;
 import org.game.unit.GameUnit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class FortificationProcessor implements FortificationService{
+
     private final NamesRandomizer namesRandomizer;
+
     public FortificationProcessor() {
         namesRandomizer = new NamesRandomizer();
     }
 
-    public void setNames(Map<String, GameUnit> fleet, List<Fortification> fortifications){
+    @Override
+    public void generateFortification(Map<String, GameUnit> fleet) {
+        setFortificationNames(fleet, MockedData.STANDARD_FORT_POSITION);
+    }
+    /**
+     * Accepts Surface multidimensional array and Map <String, GameUnit>
+     * void
+     * Set random names for Fortifications, update Map*/
+    @Override
+    public void getStandardFortifications(Surface[][] map, Map<String, GameUnit> fleet) {
+        ArrayList <Fortification> tmp = MockedData.STANDARD_FORT_POSITION;
+        setFortificationNames(fleet,tmp);
+        tmp.forEach(fortification -> setPortLocations(map,fortification));
+    }
+    /**
+     * Accepts Surface multidimensional array and Coordinate object
+     * Returns boolean
+     * Check if Coordinate object might be in bounds of multidimensional array*/
+    private boolean checkIfPositionValid(Surface[][] map, Coordinates coordinates){
+        if(checkValidPositionOnAxis(map,coordinates.axisX())){
+            if(checkValidPositionOnAxis(map,coordinates.axisY())){
+                return checkIfSurfaceIsWater(map[coordinates.axisX()][coordinates.axisY()]);
+            }else {
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    /**
+     * Method accepts Surface multidimensional array and int
+     * returns boolean
+     * checks if int in bounds of sub array*/
+    private boolean checkValidPositionOnAxis(Surface [][] map, int index){
+        return index >= 0 && index < map[0].length;
+    }
+    /**
+     * Method accepts Surface object
+     * returns boolean
+     * returns true if SurfaceType of Surface object is WATER*/
+    private boolean checkIfSurfaceIsWater(Surface surface){
+        return surface.getType() == SurfaceType.WATER;
+    }
+    /**
+     * Method accepts Map and List of Fortification Objects
+     * sets a random name to each object
+     * fulfill Map with Object's id as a key and Object itself as a value*/
+    private void setFortificationNames(Map<String, GameUnit> fleet, List<Fortification> fortifications){
         fortifications.forEach(e->{
             if(e.getFortificationType()== FortificationType.ROYAL_PORT){
                 e.setId(namesRandomizer.royalPortNames.pop());
@@ -26,10 +82,17 @@ public class FortificationProcessor implements FortificationService{
             }
         });
     }
+    /**
+     * Method accepts Surface array and Fortification object
+     * switches SurfaceType to PORT
+     * adds Surface to port Object's field */
+    private void setPortLocations(Surface[][] map, Fortification fortification) {
+        Arrays.stream(CardinalPoint.cardinalPoints).forEach(cardinalPoint -> {
+            if (checkIfPositionValid(map,new Coordinates(fortification.getCoordinates().axisX()+cardinalPoint.getValue().axisX(),fortification.getCoordinates().axisY()+cardinalPoint.getValue().axisY()))){
+                map[fortification.getCoordinates().axisX()+cardinalPoint.getValue().axisX()][fortification.getCoordinates().axisY()+cardinalPoint.getValue().axisY()].setType(SurfaceType.PORT);
+                fortification.getPort().add(map[fortification.getCoordinates().axisX()+cardinalPoint.getValue().axisX()][fortification.getCoordinates().axisY()+cardinalPoint.getValue().axisY()]);
+            }
+        });
 
-    @Override
-    public void generateFortification(Map<String, GameUnit> fleet) {
-        setNames(fleet, MockedData.STANDARD_FORT_POSITION);
     }
-
 }

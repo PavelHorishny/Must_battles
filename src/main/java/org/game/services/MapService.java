1ba627f9ba525de43902bf7927fb1d478cc479c8
Pavel Horishny <horishnypavel@gmail.com>
2024-11-21 01:02:28 +0100

    refactored map service

--- src/main/java/org/game/services/MapService.java
+++ src/main/java/org/game/services/MapService.java
@@ -1,12 +1,11 @@
 package org.game.services;
 
-import org.game.State;
+
 import org.game.map.Surface;
-import org.game.unit.Fortification;
+
 
 public interface MapService {
     Surface [] [] generateStandardMap();
-    void setPortLocations(Surface [][] map, Fortification fortification);
     void getRandomMap();
-
+    //TODO remake generateStandardMap (Surface [][] map) <- change signature
 }

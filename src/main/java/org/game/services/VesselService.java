b321e40578f4d8469d88694f8f51aa80bf7e15e4
Pavel Horishny <horishnypavel@gmail.com>
2024-11-21 01:11:12 +0100

    get vessels method to vessel service added

--- src/main/java/org/game/services/VesselService.java
+++ src/main/java/org/game/services/VesselService.java
@@ -1,4 +1,10 @@
 package org.game.services;
 
+import org.game.map.Surface;
+import org.game.unit.GameUnit;
+
+import java.util.Map;
+
 public interface VesselService {
+    void getVessels(Map<String, GameUnit> fleet, Surface [][] map);
 }

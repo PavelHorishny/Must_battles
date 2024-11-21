235d821c125c44cd7d98f9faca7b8da58c558b87
Pavel Horishny <horishnypavel@gmail.com>
2024-11-21 01:07:22 +0100

    server first message

--- src/main/java/org/game/messaging/GameServer.java
+++ src/main/java/org/game/messaging/GameServer.java
@@ -2,10 +2,18 @@ package org.game.messaging;
 
 import org.game.*;
 import org.game.gui.panels.Message;
+import org.game.map.Surface;
 import org.game.mockData.StandardMap;
+import org.game.services.MapProcessor;
+import org.game.services.MapService;
+import org.game.services.UnitProcessor;
+import org.game.services.UnitService;
 
 public class GameServer implements Server{
-    State state = new State();
+    State state;
+    Surface [] [] map;
+    MapService mapProcessor = new MapProcessor();
+    private final UnitService unitService = new UnitProcessor();
     public GameServer() {
         Context.setServer(this);
         System.out.println("Server exists");
@@ -15,11 +23,14 @@ public class GameServer implements Server{
     public void onRequest(Client client, Message message) {
         switch (message){
             case START -> {
+                state = unitService.initialGameState();
+                //map = mapProcessor.generateStandardMap();
                 //state.setMapAreaState(new MapAreaState().setMap(StandardMap.MAP));
-                state.setMapAreaState(new MapAreaState(StandardMap.MAP));
+                //state.setMapAreaState(new MapAreaState(BackToGUIConverter.convertMap(state.getMap())));
                 state.setLogAreaState(new LogAreaState("Day 1"));
+                //state.update();
                 //state.setMapAreaState(state.getMapAreaState());
-                client.response(state);
+                client.response(state.update());
             }
         }
     }

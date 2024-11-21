3a06f33019c3e68b786317e83169530035743de3
Pavel Horishny <horishnypavel@gmail.com>
2024-11-21 01:08:34 +0100

    refactored FortificationService

--- src/main/java/org/game/services/FortificationService.java
+++ src/main/java/org/game/services/FortificationService.java
@@ -1,5 +1,7 @@
 package org.game.services;
 
+import org.game.map.Surface;
+import org.game.unit.Fortification;
 import org.game.unit.GameUnit;
 
 import java.util.Map;
@@ -7,4 +9,7 @@ import java.util.Map;
 public interface FortificationService {
     //public void setNames(Map<String, GameUnit> fleet, List<Fortification> fortifications);
     public void generateFortification(Map<String,GameUnit> fleet);
+
+    //void setPortLocations(Surface[][] map);
+    void getStandardFortifications(Surface [][] map, Map<String, GameUnit> fleet);
 }

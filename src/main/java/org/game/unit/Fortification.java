ad5431bd5bd1f4b0e090437f2da07e35af22197a
Pavel Horishny <horishnypavel@gmail.com>
2024-11-21 18:21:56 +0100

    changed access modifier

--- src/main/java/org/game/unit/Fortification.java
+++ src/main/java/org/game/unit/Fortification.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 public class Fortification extends GameUnit {
-    private final FortificationType fortificationType;
+    private FortificationType fortificationType;
     private List<Surface> port = new ArrayList<>();
 
     public Fortification(Coordinates coordinates,FortificationType fortificationType,boolean player){

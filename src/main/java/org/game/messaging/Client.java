245900431315391b20e0463acfbce974e6f6e3c8
Pavel Horishny <horishnypavel@gmail.com>
2024-12-03 00:45:40 +0100

    change request method signature

--- src/main/java/org/game/messaging/Client.java
+++ src/main/java/org/game/messaging/Client.java
@@ -1,11 +1,12 @@
 package org.game.messaging;
 
+import org.game.Request;
 import org.game.State;
 import org.game.gui.panels.Mediator;
-import org.game.gui.panels.Message;
+
 
 public interface Client {
-    void request(Message message);
+    void request(Request request);
     void connect(Server server);
     void registerMediator(Mediator mediator);
     void response(State state);

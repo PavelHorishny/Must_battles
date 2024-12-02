cb2d7576ca5bef991a36455b668c06b34d5e3ffc
Pavel Horishny <horishnypavel@gmail.com>
2024-12-02 19:30:58 +0100

    updated Constants

--- src/main/java/org/game/gui/Constants.java
+++ src/main/java/org/game/gui/Constants.java
@@ -6,7 +6,6 @@ public class Constants {
     public static final int CELL_SIZE = 30;
     public static final Color WATER = Color.BLUE;
     public static final Color LAND = Color.GREEN.darker().darker();
-    public static final Color ROUTE = WATER.darker();
-
-    public static final Color PORT = WATER.brighter();
+    public static final Color ROUTE = WATER.darker().darker();
+    public static final Color PORT = WATER.darker();
 }

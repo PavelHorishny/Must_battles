da1d51e3106fcf3537b50923f13a41d70a2af5fd
Pavel Horishny <horishnypavel@gmail.com>
2024-12-02 19:22:10 +0100

    VesselProcessor refactoring

--- src/main/java/org/game/services/VesselProcessor.java
+++ src/main/java/org/game/services/VesselProcessor.java
@@ -1,5 +1,6 @@
 package org.game.services;
 
+import org.game.Context;
 import org.game.map.Surface;
 import org.game.mockData.MockedData;
 import org.game.mockData.NamesRandomizer;
@@ -8,43 +9,36 @@ import org.game.unit.*;
 import java.util.*;
 
 public class VesselProcessor implements VesselService{
-    NamesRandomizer namesRandomizer = new NamesRandomizer();
+    NamesRandomizer namesRandomizer = Context.getNameRandomizer();
     ArrayList <Vessel> firstPlayerVessels = new ArrayList<>();
+
+    public VesselProcessor(){
+        //namesRandomizer = new NamesRandomizer();
+    }
+    /**
+     * Accepts Map<String,GameUnit> to keep added vessels objects, Surface [][] that keeps map
+     * Void
+     * Method calls setFleet with necessary parameters*/
+
     @Override
     public void getVessels(Map<String, GameUnit> fleet, Surface[][] map) {
-        //Stack <Vessel> firstPlayerFleet = randomizeFleet(generateVesselsWithNames(true));
-        //Stack <Vessel> secondPlayerFleet = randomizeFleet(generateVesselsWithNames(false));
         setFleet(randomizeFleet(generateVesselsWithNames(true)),randomizeFleet(generateVesselsWithNames(false)),fleet,map);
-        /*{{
-            add(new Vessel(true, VesselType.THREE_DECKER_SHIP_OF_LINE));
-            add(new Vessel(true,VesselType.THREE_DECKER_SHIP_OF_LINE));
-        }};*/
     }
+    /**
+     * Accepts Stack<Vessel> that keeps vessel objects in random order for first player,
+     *      Stack<Vessel> that keeps vessel objects in random order for second player, Map<String,GameUnit> to keep game unit objects and Surface [][] that keeps map
+     *Void
+     * Method calls setVesselsInFort with necessary parameters for each player*/
 
     private void setFleet(Stack<Vessel> firstPlayerFleet, Stack<Vessel> secondPlayerFleet, Map<String, GameUnit> fleet, Surface[][] map) {
-        //sortFortifications(fleet);
         setVesselsInFort(true,map,fleet,firstPlayerFleet);
         setVesselsInFort(false,map,fleet,secondPlayerFleet);
-/*        fleet.values().stream().filter(GameUnit::isFirstPlayer).filter(unit -> unit.getUnitType().equals(UnitType.FORTIFICATION)).map(Fortification.class::cast).forEach(fortification -> {
-            switch (fortification.getFortificationType()){
-                case FIRST_LINE_FORT ->
-                    setVesselsInPort(4,fortification,map,fleet,firstPlayerFleet);
-
-                case SECOND_LINE_FORT ->
-                    setVesselsInPort(5,fortification,map,fleet,firstPlayerFleet);
-            }
-        });
-
-        fleet.values().stream().filter(unit -> !unit.isFirstPlayer()).filter(unit -> unit.getUnitType().equals(UnitType.FORTIFICATION)).map(Fortification.class::cast).forEach(fortification -> {
-            switch (fortification.getFortificationType()){
-                case FIRST_LINE_FORT ->
-                    setVesselsInPort(4,fortification,map,fleet,secondPlayerFleet);
-
-                case SECOND_LINE_FORT ->
-                    setVesselsInPort(5,fortification,map,fleet,secondPlayerFleet);
-            }
-        });*/
     }
+    /**
+     * Accepts int that represents quantity of vessels in fortification of given type, Fortification where vessels will be placed, Surface [][] that keeps map,
+     *  Map<String,GameUnit> where vessel object will keep, Stack<Vessel> that keeps Vessel objects in random order
+     * Void
+     * Method allocates Vessels objects in port field of Surface object and adds vessel object to Map<String,GameUnit> in int quantity according to game rules */
 
     private void setVesselsInPort(int size, Fortification fortification, Surface [][] map, Map<String,GameUnit> fleet, Stack<Vessel> playerFleet){
         for(int i = 0; i<size; i++){
@@ -54,9 +48,16 @@ public class VesselProcessor implements VesselService{
             fleet.put(tmp.getId(),tmp);
         }
     }
+    /**
+     * Accepts boolean that represents player, Surface [][] that keeps map, Map<String, GameUnit> that keeps Fortification objects and Stack <Vessel> of vessels in random order
+     * Void
+     * Method filters Map<String,GameUnit> by given player, that according to Fortification type calls setVesselsInPort method with necessary parameters
+     * and allocates Vessel objects from Stack<Vessel>*/
+
     private void setVesselsInFort(boolean isFirstPlayer,Surface [][] map,Map<String,GameUnit> fleet,Stack<Vessel> vessels){
         fleet.values().stream().filter(unit -> unit.isFirstPlayer()==isFirstPlayer).filter(unit -> unit.getUnitType().equals(UnitType.FORTIFICATION))
-                .map(Fortification.class::cast).forEach(fortification -> {
+                .map(Fortification.class::cast).toList()
+                .forEach(fortification -> {
                     switch (fortification.getFortificationType()){
                         case FIRST_LINE_FORT ->
                             setVesselsInPort(4,fortification,map,fleet,vessels);
@@ -73,7 +74,12 @@ public class VesselProcessor implements VesselService{
                         map[fortification.getPort().get(5).getCoordinates().axisX()][fortification.getPort().get(5).getCoordinates().axisY()].setUnit(tmp);
                     }
                 });
+        //TODO find way to simplify last part of method but consider that it is possible that more than one Fortification can have port size bigger than 5
     }
+    /**
+     * Accepts ArrayList<Vessel>
+     * Returns Stack<Vessel>
+     * Method takes an arrayList of vessels and fills a Stack in random order*/
 
     private Stack<Vessel> randomizeFleet(ArrayList<Vessel> vessels){
         Stack<Vessel> fleet = new Stack<>();
@@ -86,72 +92,47 @@ public class VesselProcessor implements VesselService{
 
         return fleet;
     }
+    /**
+     * Accepts boolean that indicates player
+     * Returns ArrayList<Vessel>
+     * Method uses array of all vessel types to create all necessary vessel units for given player*/
+
     private ArrayList<Vessel> generateVesselsWithNames(boolean isFirstPlayer){
         ArrayList<Vessel> vessels = new ArrayList<>();
-        Arrays.stream(VesselType.vesselTypes).forEach(vesselType -> {
-            switch (vesselType){
-                case THREE_DECKER_SHIP_OF_LINE -> {
-                    for(int i = 0; i< MockedData.THREE_DECKER_SHIP_THE_LINE_QNT/2; i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.bigBattleshipsNames.pop()));
-                    }
-                }
-                case TWO_DECKER_SHIP_OF_LINE -> {
-                    for(int i = 0; i<MockedData.TWO_DECKER_SHIP_THE_LINE_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.smallBattleshipsNames.pop()));
-                    }
-                }
-                case FRIGATE -> {
-                    for(int i=0;i<MockedData.FRIGATES_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.frigatesNames.pop()));
-                    }
-                }
-                case TENDER -> {
-                    for(int i = 0; i<MockedData.TENDERS_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.tendersNames.pop()));
-                    }
-                }
-                case BRIG -> {for(int i = 0; i<MockedData.BRIGS_QNT/2;i++){
-                    vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.brigNames.pop()));
-                }
-                }
-                case GALLEON -> {
-                    for(int i = 0; i<MockedData.GALLEONS_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.galleonsNames.pop()));
-                    }
-                }
-                case STEAM_FRIGATE -> {
-                    for(int i = 0; i<MockedData.STEAM_FRIGATE_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.steamFrigateNames.pop()));
-                    }
-                }
-                case BATTERY -> {
-                    for(int i = 0; i<MockedData.NAVAL_BATTERY_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.navalBatteryNames.pop()));
-                    }
-                }
-                case GALLEY -> {
-                    for(int i = 0; i<MockedData.GALLEYS_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.galleysName.pop()));
-                    }
-                }
-                case CORVETTE -> {
-                    for(int i = 0; i<MockedData.STEAM_CORVETTE_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.steamCorvetteNames.pop()));
-                    }
-                }
-                case MONITOR -> {
-                    for(int i = 0; i<MockedData.MONITOR_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.monitorNames.pop()));
-                    }
-                }
-                case STEAMSHIP -> {
-                    for(int i = 0; i<MockedData.STEAMSHIP_QNT/2;i++){
-                        vessels.add(new Vessel(isFirstPlayer,vesselType,namesRandomizer.steamshipNames.pop()));
-                    }
-                }
-            }
-        });
+        Arrays.stream(VesselType.vesselTypes).forEach(vesselType -> addVesselsByType(vesselType,vessels,isFirstPlayer));
         return vessels;
+    }
+    /**
+     * Accepts VesselType, ArrayList<Vessel> that will be modified,boolean that indicates player
+     * Void
+     * Method calls addVesselsWithNames method for given VesselType, and passes necessary parameters for given VesselType*/
+
+    private void addVesselsByType(VesselType vesselType, ArrayList<Vessel> vessels, boolean isFirstPlayer){
+        switch (vesselType){
+            case THREE_DECKER_SHIP_OF_LINE -> addVesselsWithNames(vessels,MockedData.THREE_DECKER_SHIP_THE_LINE_QNT/2,isFirstPlayer,namesRandomizer.bigBattleshipsNames,vesselType);
+            case TWO_DECKER_SHIP_OF_LINE -> addVesselsWithNames(vessels,MockedData.TWO_DECKER_SHIP_THE_LINE_QNT/2,isFirstPlayer,namesRandomizer.smallBattleshipsNames,vesselType);
+            case FRIGATE -> addVesselsWithNames(vessels,MockedData.FRIGATES_QNT/2,isFirstPlayer,namesRandomizer.frigatesNames,vesselType);
+            case TENDER -> addVesselsWithNames(vessels,MockedData.TENDERS_QNT/2,isFirstPlayer,namesRandomizer.tendersNames,vesselType);
+            case BRIG -> addVesselsWithNames(vessels,MockedData.BRIGS_QNT/2,isFirstPlayer,namesRandomizer.brigNames,vesselType);
+            case GALLEON -> addVesselsWithNames(vessels,MockedData.GALLEONS_QNT/2,isFirstPlayer,namesRandomizer.galleonsNames,vesselType);
+            case STEAM_FRIGATE -> addVesselsWithNames(vessels,MockedData.STEAM_FRIGATE_QNT/2,isFirstPlayer,namesRandomizer.steamFrigateNames,vesselType);
+            case BATTERY -> addVesselsWithNames(vessels,MockedData.NAVAL_BATTERY_QNT/2,isFirstPlayer,namesRandomizer.navalBatteryNames,vesselType);
+            case GALLEY -> addVesselsWithNames(vessels,MockedData.GALLEYS_QNT/2,isFirstPlayer,namesRandomizer.galleysName,vesselType);
+            case CORVETTE -> addVesselsWithNames(vessels,MockedData.STEAM_CORVETTE_QNT/2,isFirstPlayer,namesRandomizer.steamCorvetteNames,vesselType);
+            case MONITOR -> addVesselsWithNames(vessels,MockedData.MONITOR_QNT/2,isFirstPlayer,namesRandomizer.monitorNames,vesselType);
+            case STEAMSHIP -> addVesselsWithNames(vessels,MockedData.STEAMSHIP_QNT/2,isFirstPlayer,namesRandomizer.steamshipNames,vesselType);
+        }
+    }
+    /**
+     * Accepts ArrayList<Vessel> that must be filled, int quantity of iterations that depends on quantity of vessels that player should have of certain type,
+     * boolean that indicates player, Stack<String> that contains names for certain vessel type, VesselType
+     * void
+     * Method adds to ArrayList<Vessel> new Vessel objects of certain VesselType in given quantity and sets names for it taken from Stack <String> of mocked names*/
+
+    private void addVesselsWithNames(ArrayList<Vessel> vessels,int qnt,boolean player,Stack<String> names,VesselType vesselType){
+        for (int i = 0; i<qnt; i++){
+            vessels.add(new Vessel(player,vesselType,names.pop()));
+        }
     }
     /**
      * Each player has 3 first line forts 6 in total
@@ -170,5 +151,5 @@ public class VesselProcessor implements VesselService{
      * Each player has 1 Monitor 2 in total
      * Each player has 1 Steamship 2 in total
      * */
-    //TODO separate methods convert array to stack complete vessel generation
+    //TODO test for vessel processor
 }

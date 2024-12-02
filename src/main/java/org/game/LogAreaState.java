c6c4be5df565bc3cc6264307f869fda6ec6981d6
Pavel Horishny <horishnypavel@gmail.com>
2024-12-03 00:59:56 +0100

    refactored

--- src/main/java/org/game/LogAreaState.java
+++ src/main/java/org/game/LogAreaState.java
@@ -1,10 +1,12 @@
 package org.game;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class LogAreaState extends GameComponentState{
     String label;
     public LogAreaState() {

b88c131dab0999058a0f265de3df26f4bf4b44fc
Pavel Horishny <horishnypavel@gmail.com>
2024-12-03 00:46:58 +0100

    request class added

---
+++ src/main/java/org/game/Request.java
@@ -0,0 +1,13 @@
+package org.game;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.game.gui.panels.Message;
+@Getter
+@Setter
+@Builder
+public class Request {
+    Message message;
+    String id;
+}

513fa6dabb7c0bd6436e0d0c94bee28acaf95ed4
Pavel Horishny <horishnypavel@gmail.com>
2024-12-20 20:41:55 +0100

    changed layout to cardLayout

--- src/main/java/org/game/gui/panels/game/components/InfoArea.java
+++ src/main/java/org/game/gui/panels/game/components/InfoArea.java
@@ -1,14 +1,22 @@
 package org.game.gui.panels.game.components;
 
+import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.game.GameComponentState;
+import org.game.gui.panels.InfoAreaPanels.SelectedInfoPanel;
+import org.game.state.GameComponentState;
+import org.game.gui.panels.InfoAreaPanels.GeneralInfoPanel;
 import org.game.gui.panels.Settings;
+import org.game.state.InfoAreaState;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class InfoArea extends GamePanelComponent {
     private static final Logger logger = LogManager.getLogger(InfoArea.class);
+    @Getter
+    private final GeneralInfoPanel generalInfoPanel = new GeneralInfoPanel();
+    private final SelectedInfoPanel selectedInfoPanel = new SelectedInfoPanel();
     public JLabel l = new JLabel();
     public JLabel hit_points = new JLabel();
     String textHP = "hit points: ";
@@ -19,12 +27,15 @@ public class InfoArea extends GamePanelComponent {
     public InfoArea(Settings settings) {
 
         super(settings);
-        l.setText(name);
+        setLayout(new CardLayout());
+        add(generalInfoPanel,"General");
+        add(selectedInfoPanel,"Selected");
+   /*     l.setText(name);
         setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
         add(l);
         add(hit_points);
         add(shots);
-        add(movePoints);
+        add(movePoints);*/
         /*bu*/
         logger.debug(name);
     }
@@ -40,6 +51,11 @@ public class InfoArea extends GamePanelComponent {
 
     @Override
     public void updateState(GameComponentState state) {
-
+        InfoAreaState s = (InfoAreaState) state;
+        generalInfoPanel.getDay().setText("Day "+s.getDay());
+        if(s.isSelected()){
+            ((CardLayout) this.getLayout()).show(this,"Selected");
+            selectedInfoPanel.setState(((InfoAreaState) state).getSelectedData());
+        }
     }
 }

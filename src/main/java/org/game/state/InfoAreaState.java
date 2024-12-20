0bf76a0cc7b6482702284ec784a58f59d6960dc7
Pavel Horishny <horishnypavel@gmail.com>
2024-12-20 20:42:23 +0100

    InfoAreaState added

---
+++ src/main/java/org/game/state/InfoAreaState.java
@@ -0,0 +1,15 @@
+package org.game.state;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.game.UnitData;
+
+@Getter
+@Setter
+@Builder
+public class InfoAreaState extends GameComponentState {
+    private String day = "";
+    private boolean selected;
+    private UnitData selectedData;
+}

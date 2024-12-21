84c2e086539449d76958123b8510303679e94629
Pavel Horishny <horishnypavel@gmail.com>
2024-12-21 23:17:30 +0200

    implemented unitSelected() method

--- src/main/java/org/game/services/UnitProcessor.java
+++ src/main/java/org/game/services/UnitProcessor.java
@@ -1,8 +1,8 @@
 package org.game.services;
 
-import lombok.Getter;
 import org.game.BackToGUIConverter;
 import org.game.UnitData;
+import org.game.gui.StateType;
 import org.game.state.InfoAreaState;
 import org.game.state.MapAreaState;
 import org.game.state.State;
@@ -12,15 +12,18 @@ import org.game.unit.GameUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 
-@Getter
+
 public class UnitProcessor implements UnitService{
     private Surface[][] map;
     private ArrayList <Surface> route; // maybe redundant
     private final Map <String, GameUnit> fleet = new HashMap<>();
     private int day;
-
+    private boolean isFirstPlayerMove;
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    private Optional <GameUnit> selected = Optional.empty();
     private final MapService mapProcessor = new MapProcessor();
     private final FortificationService fortificationProcessor = new FortificationProcessor();
     private final VesselProcessor vesselProcessor = new VesselProcessor();
@@ -36,6 +39,7 @@ public class UnitProcessor implements UnitService{
     @Override
     public State initialGameState() {
         day = 1;
+        isFirstPlayerMove = true;
         map = mapProcessor.generateStandardMap();
         //fortificationProcessor.setPortLocations(map);
         fortificationProcessor.getStandardFortifications(map,fleet);
@@ -48,11 +52,38 @@ public class UnitProcessor implements UnitService{
 
     @Override
     public State unitSelected(String id) {
-        GameUnit unit = fleet.get(id);
-        UnitData data = new UnitData(unit.getType(), unit.getId(), unit.getHit_points(), unit.getHit_points(), unit.getFire_range(), unit.getShots());
-        System.out.println(unit.getId());
-        System.out.println(unit.getHit_points());
-        return State.builder().mapAreaState(MapAreaState.builder().map(BackToGUIConverter.convertMap(map)).fleet(BackToGUIConverter.convertFleet(fleet)).build())
-                .infoAreaState(InfoAreaState.builder().day(String.valueOf(day)).selected(true).selectedData(data).build()).build();
+        /**
+         * if id is not empty
+         * get unit by id
+         * check if unit is belonged to player 
+         * if so check is unit is already selected
+         * if not replace optional and change state*/
+        if(!id.isEmpty()){
+            GameUnit unit = fleet.get(id);
+            if(unit.isFirstPlayer()==isFirstPlayerMove){
+                if(selected.isPresent()){
+                    if(!selected.get().equals(unit)){
+                        selected.get().setStateType(StateType.PASSIVE);
+                        selected = Optional.empty();
+                    }
+                }
+                unit.setStateType(StateType.SELECTED);
+                selected = Optional.of(unit);
+                UnitData data = unit.toUnitData();
+                return State.builder().mapAreaState(MapAreaState.builder().map(BackToGUIConverter.convertMap(map)).fleet(BackToGUIConverter.convertFleet(fleet)).build())
+                        .infoAreaState(InfoAreaState.builder().day(String.valueOf(day)).selected(true).selectedData(data).build()).build();
+            }else{
+                UnitData data = unit.toUnitData();
+                return State.builder().mapAreaState(MapAreaState.builder().map(BackToGUIConverter.convertMap(map)).fleet(BackToGUIConverter.convertFleet(fleet)).build())
+                        .infoAreaState(InfoAreaState.builder().day(String.valueOf(day)).target(true).targetData(data).build()).build();
+            }
+        }else{
+            if(selected.isPresent()){
+                selected.get().setStateType(StateType.PASSIVE);
+                selected = Optional.empty();
+            }
+            return State.builder().mapAreaState(MapAreaState.builder().map(BackToGUIConverter.convertMap(map)).fleet(BackToGUIConverter.convertFleet(fleet)).build())
+                    .infoAreaState(InfoAreaState.builder().day(String.valueOf(day)).selected(false).build()).build();
+        }
     }
 }

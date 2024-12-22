25c544916643ef8fdf62b9487a2b48f35f8564f2
Pavel Horishny <horishnypavel@gmail.com>
2024-12-22 17:06:20 +0200

    SelectedInfoPanel changed visual appearance

--- src/main/java/org/game/gui/panels/InfoAreaPanels/SelectedInfoPanel.java
+++ src/main/java/org/game/gui/panels/InfoAreaPanels/SelectedInfoPanel.java
@@ -3,25 +3,28 @@ package org.game.gui.panels.InfoAreaPanels;
 import org.game.UnitData;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SelectedInfoPanel extends JPanel {
-    private JLabel type = new JLabel("type");
-    private JLabel name = new JLabel("name");
-    private JLabel hitPoints = new JLabel("1");
-    private JLabel movePoints = new JLabel("1");
-    private JLabel firePoints = new JLabel("1");
+
+    private final SolidTextComponent name = new SolidTextComponent();
+    private final SolidTextComponent type = new SolidTextComponent();
+    private final MixedComponent hit_points = new MixedComponent("HP", Color.GREEN);
+    private final MixedComponent shots = new MixedComponent("Shots",Color.GREEN);
+    private final ImageComponent testName5 = new ImageComponent();
+
     public SelectedInfoPanel(){
-        add(type);
         add(name);
-        add(hitPoints);
-        add(movePoints);
-        add(firePoints);
+        add(type);
+        add(hit_points);
+        add(shots);
+        add(testName5);
     }
+
     public void setState(UnitData data){
-        type.setText(data.type());
         name.setText(data.name());
-        hitPoints.setText(String.valueOf(data.current_hit_points()));
-        movePoints.setText(String.valueOf(data.move_points()));
-        firePoints.setText(String.valueOf(data.fire_range()));
+        type.setText(data.type());
+        hit_points.setData(data.base_hit_points(),data.current_hit_points());
+        shots.setData(data.base_shots(),data.current_shots());
     }
 }

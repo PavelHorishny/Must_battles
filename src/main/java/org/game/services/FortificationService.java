ecedb9dbb86b552b85aea7bdaef3329de5675d35
Pavel Horishny <horishnypavel@gmail.com>
2024-12-23 11:41:35 +0200

    To FortificationService interface setPortLocations method was added

--- src/main/java/org/game/services/FortificationService.java
+++ src/main/java/org/game/services/FortificationService.java
@@ -5,11 +5,13 @@ import org.game.unit.Fortification;
 import org.game.unit.GameUnit;
 
 import java.util.Map;
+import java.util.Set;
 
 public interface FortificationService {
     //public void setNames(Map<String, GameUnit> fleet, List<Fortification> fortifications);
-    public void generateFortification(Map<String,GameUnit> fleet);
-
+    //public void generateFortification(Map<String,GameUnit> fleet);
     //void setPortLocations(Surface[][] map);
+
     void getStandardFortifications(Surface [][] map, Map<String, GameUnit> fleet);
+    void setPortLocations(Set<Surface> port, Fortification fortification) ;
 }

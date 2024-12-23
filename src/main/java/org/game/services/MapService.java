a69c5f000aec57ecb9254af9cfc67cfa34f803c0
Pavel Horishny <horishnypavel@gmail.com>
2024-12-23 11:48:25 +0200

    getPort, getRoute and clearRoute methods were added to MapService interface

--- src/main/java/org/game/services/MapService.java
+++ src/main/java/org/game/services/MapService.java
@@ -1,11 +1,18 @@
 package org.game.services;
 
 
+import org.game.gui.Coordinates;
 import org.game.map.Surface;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 
 public interface MapService {
     Surface [] [] generateStandardMap();
     void getRandomMap();
+    Set<Surface> getPort(Coordinates coordinates, Surface [] [] map);
+    void getRoute(Coordinates coordinates, int current_move_points, ArrayList<Surface> route,Surface [][] map);
+    void clearRoute(ArrayList<Surface> route);
     //TODO remake generateStandardMap (Surface [][] map) <- change signature
 }

25dad2eb711ef83c47aed4266772ed6710e2e476
Pavel Horishny <horishnypavel@gmail.com>
2024-12-23 11:54:21 +0200

    initialGameState method implementation was updated according to changes in Map and Fortification processors. UnitSelected method implementation was updated

--- src/main/java/org/game/services/UnitProcessor.java
+++ src/main/java/org/game/services/UnitProcessor.java
@@ -7,6 +7,7 @@ import org.game.state.InfoAreaState;
 import org.game.state.MapAreaState;
 import org.game.state.State;
 import org.game.map.Surface;
+import org.game.unit.Fortification;
 import org.game.unit.GameUnit;
 
 import java.util.ArrayList;
@@ -18,7 +19,7 @@ import java.util.Optional;
 
 public class UnitProcessor implements UnitService{
     private Surface[][] map;
-    private ArrayList <Surface> route; // maybe redundant
+    private final ArrayList <Surface> route = new ArrayList<>(); // maybe redundant
     private final Map <String, GameUnit> fleet = new HashMap<>();
     private int day;
     private boolean isFirstPlayerMove;
@@ -43,6 +44,7 @@ public class UnitProcessor implements UnitService{
         map = mapProcessor.generateStandardMap();
         //fortificationProcessor.setPortLocations(map);
         fortificationProcessor.getStandardFortifications(map,fleet);
+        fleet.values().stream().map(Fortification.class::cast).toList().forEach(fortification -> fortificationProcessor.setPortLocations(mapProcessor.getPort(fortification.getCoordinates(),map),fortification));
         vesselProcessor.getVessels(fleet,map);
         //fleet.values().stream().map(Fortification.class::cast).toList().forEach(u-> System.out.println(u.getPort().size()+" "+u.getCoordinates().toString()));
         return State.builder().mapAreaState(MapAreaState.builder().map(BackToGUIConverter.convertMap(map)).fleet(BackToGUIConverter.convertFleet(fleet)).build())
@@ -60,15 +62,20 @@ public class UnitProcessor implements UnitService{
          * if not replace optional and change state*/
         if(!id.isEmpty()){
             GameUnit unit = fleet.get(id);
+            System.out.println(map[unit.getCoordinates().axisX()][unit.getCoordinates().axisY()].getUnit());
+            System.out.println("watch");
             if(unit.isFirstPlayer()==isFirstPlayerMove){
+                mapProcessor.getRoute(unit.getCoordinates(), unit.getMovePoints(), route,map);
                 if(selected.isPresent()){
                     if(!selected.get().equals(unit)){
+                        mapProcessor.clearRoute(route);
                         selected.get().setStateType(StateType.PASSIVE);
                         selected = Optional.empty();
                     }
                 }
                 unit.setStateType(StateType.SELECTED);
                 selected = Optional.of(unit);
+                mapProcessor.getRoute(unit.getCoordinates(), unit.getMovePoints(), route,map);
                 UnitData data = unit.toUnitData();
                 return State.builder().mapAreaState(MapAreaState.builder().map(BackToGUIConverter.convertMap(map)).fleet(BackToGUIConverter.convertFleet(fleet)).build())
                         .infoAreaState(InfoAreaState.builder().day(String.valueOf(day)).selected(true).selectedData(data).build()).build();

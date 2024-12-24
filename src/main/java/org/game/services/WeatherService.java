107e2976ebdff1ed2b4b9d9770346decf3d5cf66
Pavel Horishny <horishnypavel@gmail.com>
2024-12-24 11:29:13 +0200

    getPenalty method added

--- src/main/java/org/game/services/WeatherService.java
+++ src/main/java/org/game/services/WeatherService.java
@@ -1,7 +1,9 @@
 package org.game.services;
 
+import org.game.CardinalPoint;
 import org.game.Weather;
 
 public interface WeatherService {
     Weather getWeather();
+    int getPenalty(CardinalPoint windDirection, CardinalPoint routeDirection);
 }

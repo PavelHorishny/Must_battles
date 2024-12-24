c8ab5e83f4c2058bafbcc2a4080a4c2ff3ad9aa6
Pavel Horishny <horishnypavel@gmail.com>
2024-12-24 11:48:02 +0200

    getRoute method updated

--- src/main/java/org/game/services/MapService.java
+++ src/main/java/org/game/services/MapService.java
@@ -3,6 +3,7 @@ package org.game.services;
 
 import org.game.gui.Coordinates;
 import org.game.map.Surface;
+import org.game.unit.Vessel;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -12,7 +13,7 @@ public interface MapService {
     Surface [] [] generateStandardMap();
     void getRandomMap();
     Set<Surface> getPort(Coordinates coordinates, Surface [] [] map);
-    void getRoute(Coordinates coordinates, int current_move_points, ArrayList<Surface> route,Surface [][] map);
+    void getRoute(Vessel vessel, ArrayList<Surface> route, Surface [][] map);
     void clearRoute(ArrayList<Surface> route);
     //TODO remake generateStandardMap (Surface [][] map) <- change signature
 }

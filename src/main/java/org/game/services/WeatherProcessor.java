6110af17b1699070dab00b02bb8a5b9f19ee6e2a
Pavel Horishny <horishnypavel@gmail.com>
2024-12-24 11:43:43 +0200

    getPenalty method implemented

--- src/main/java/org/game/services/WeatherProcessor.java
+++ src/main/java/org/game/services/WeatherProcessor.java
@@ -1,6 +1,7 @@
 package org.game.services;
 
 import org.game.CardinalPoint;
+import org.game.CardinalPointsNet;
 import org.game.Weather;
 import org.game.Wind;
 
@@ -16,5 +17,29 @@ public class WeatherProcessor implements WeatherService{
         CardinalPoint cardinalPoint = CardinalPoint.cardinalPoints[random.nextInt(CardinalPoint.cardinalPoints.length)];
         Wind wind = Wind.windForce[random.nextInt(Wind.windForce.length)];
         return new Weather(cardinalPoint,wind);
+    }
+
+    /**
+     * @param windDirection 
+     * @param routeDirection
+     * @return
+     */
+    @Override
+    public int getPenalty(CardinalPoint windDirection, CardinalPoint routeDirection) {
+        int result = 0;
+        CardinalPoint left = CardinalPointsNet.pointers.get(windDirection).getLeft();
+        CardinalPoint right = CardinalPointsNet.pointers.get(windDirection).getRight();
+        if(windDirection==routeDirection){
+            return result;
+        }
+        for (int i = 1; i < CardinalPointsNet.pointers.size(); i++) {
+            if (left == routeDirection || right == routeDirection) {
+                result=i;
+                break;
+            }
+            left = CardinalPointsNet.pointers.get(left).getLeft();
+            right = CardinalPointsNet.pointers.get(right).getRight();
+        }
+        return result;
     }
 }

10aa9cd2cecd9468fe15a02199035d5bcc373722
Pavel Horishny <horishnypavel@gmail.com>
2024-12-26 20:24:28 +0200

    move method added

--- src/main/java/org/game/services/VesselService.java
+++ src/main/java/org/game/services/VesselService.java
@@ -1,5 +1,6 @@
 package org.game.services;
 
+import org.game.gui.Coordinates;
 import org.game.map.Surface;
 import org.game.unit.GameUnit;
 import org.game.unit.Vessel;
@@ -10,4 +11,7 @@ import java.util.Map;
 public interface VesselService {
     void setVessels(Map<String, GameUnit> fleet, Surface [][] map);
     List<Vessel> getVessels(Map <String, GameUnit> fleet);
+
+
+    void move(Vessel vessel, Coordinates destination, Surface[][] map);
 }

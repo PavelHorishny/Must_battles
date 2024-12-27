c940140cc1e094f6c05a9178d16201acc79e0555
Pavel Horishny <horishnypavel@gmail.com>
2024-12-27 19:20:42 +0200

    methods shot and salvoShot are implemented

--- src/main/java/org/game/services/FiringProcessor.java
+++ src/main/java/org/game/services/FiringProcessor.java
@@ -5,6 +5,8 @@ import org.game.unit.GameUnit;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 public class FiringProcessor implements FiringService{
     /**
@@ -25,5 +27,52 @@ public class FiringProcessor implements FiringService{
                 aimedUnits.add(unit);
             }
         });
+    }
+
+    /**
+     * @param attacker
+     * @param target
+     * @return
+     */
+    @Override
+    public Optional<GameUnit> shot(GameUnit attacker, GameUnit target) {
+            if(getHit()){
+                attacker.setCurrent_shots(attacker.getCurrent_shots()-1);
+                target.setCurrent_hit_point(target.getCurrent_hit_point()-1);
+                if(target.getCurrent_hit_point()<=0){
+                    return Optional.of(target);
+                }else{
+                    return Optional.empty();
+                }
+            }else {
+                attacker.setCurrent_shots(attacker.getCurrent_shots()-1);
+                return Optional.empty();
+            }
+    }
+
+    /**
+     * @param attacker
+     * @param target
+     * @return
+     */
+    @Override
+    public Optional<GameUnit> salvoShot(GameUnit attacker, GameUnit target) {
+        int salvo = attacker.getCurrent_shots();
+        if(getHit()){
+            attacker.setCurrent_shots(attacker.getCurrent_shots()-salvo);
+            target.setCurrent_hit_point(target.getCurrent_hit_point()-salvo);
+            if(target.getCurrent_hit_point()<=0){
+                return Optional.of(target);
+            }else{
+                return Optional.empty();
+            }
+        }else {
+            attacker.setCurrent_shots(attacker.getCurrent_shots()-salvo);
+            return Optional.empty();
+        }
+    }
+    private boolean getHit(){
+        Random random = new Random();
+        return random.nextInt(0, 1) > 0;
     }
 }

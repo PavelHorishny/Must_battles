6fe0f8c31bfc682a7e1f6da45ce572f30f0a28e2
Pavel Horishny <horishnypavel@gmail.com>
2024-12-27 12:51:03 +0200

    fixed setUnderAttack method

--- src/main/java/org/game/services/FiringService.java
+++ src/main/java/org/game/services/FiringService.java
@@ -1,9 +1,10 @@
 package org.game.services;
 
-import org.game.map.Surface;
+import org.game.unit.GameUnit;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface FiringService {
-    void setUnderAttack(List<Surface> fireZone/*, ArrayList<Surface> activeCells*/, boolean isFirstPlayer);
+    void setUnderAttack(List<GameUnit> inFiringZone, ArrayList<GameUnit> aimedUnits, boolean isFirstPlayer);
 }

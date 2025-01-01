3d5eb2b1d4e7bf159c90470119bed10529ebb347
Pavel Horishny <horishnypavel@gmail.com>
2025-01-01 16:27:26 +0200

    added new method and refactor

--- src/main/java/org/game/services/UnitService.java
+++ src/main/java/org/game/services/UnitService.java
@@ -6,12 +6,10 @@ import org.game.state.State;
 public interface UnitService {
     State initialGameState();
     State unitSelected(String id);
-
     State movementStarts(String id);
-
     State movementEnds(String id, Coordinates destination);
-
     State makeShot(String attackerID, String targetID, String shotType);
-
     State dayEnd();
+    State unitReadyForRepair(boolean state);
+    State unitReadyForHelp(boolean state);
 }

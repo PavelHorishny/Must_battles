19951c1d2dcb4d351ea05252d09627780561e15e
Pavel Horishny <horishnypavel@gmail.com>
2025-01-01 16:37:36 +0200

    added new methods and refactor

--- src/main/java/org/game/services/FortificationService.java
+++ src/main/java/org/game/services/FortificationService.java
@@ -9,14 +9,11 @@ import java.util.Map;
 import java.util.Set;
 
 public interface FortificationService {
-    //public void setNames(Map<String, GameUnit> fleet, List<Fortification> fortifications);
-    //public void generateFortification(Map<String,GameUnit> fleet);
-    //void setPortLocations(Surface[][] map);
-
     void getStandardFortifications(Surface [][] map, Map<String, GameUnit> fleet);
     void setPortLocations(Set<Surface> port, Fortification fortification);
-    //void checkFortificationsAtMoveEnd(Map <String,GameUnit> fleet, EndGame endGame);
-
     void checkFortificationsAtMoveEnd(Map<String, GameUnit> fleet, boolean player);
     void checkFortificationsAtDayEnd(Map<String,GameUnit> fleet, EndGame endGame);
+    void restoreFortificationsData(Map<String, GameUnit> fleet);
+    boolean checkIfFortificationCanBeRepaired(Fortification fortification/*, Surface[][] map*/);
+
 }

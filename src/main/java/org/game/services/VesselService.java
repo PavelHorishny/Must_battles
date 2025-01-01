a4dab2bbf6f83ce310ffcbbc1672a3c4803b1c21
Pavel Horishny <horishnypavel@gmail.com>
2025-01-01 16:25:48 +0200

    added new methods and refactor

--- src/main/java/org/game/services/VesselService.java
+++ src/main/java/org/game/services/VesselService.java
@@ -11,7 +11,9 @@ import java.util.Map;
 public interface VesselService {
     void setVessels(Map<String, GameUnit> fleet, Surface [][] map);
     List<Vessel> getVessels(Map <String, GameUnit> fleet);
-
-
+    boolean checkIfVesselCanBeRepaired(Vessel vessel, Surface [] [] map);
     void move(Vessel vessel, Coordinates destination, Surface[][] map);
+    void restoreVesselsData(Map<String, GameUnit> fleet);
+    void checkVesselsAtDayEnd(Map<String, GameUnit> fleet);
+    boolean checkIfVesselCanHelp(Vessel vessel, Surface[][] map);
 }

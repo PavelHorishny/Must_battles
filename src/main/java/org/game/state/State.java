497ce55e241701e3cd3b78fc840cc3f5532b42f3
Pavel Horishny <horishnypavel@gmail.com>
2025-01-04 14:18:06 +0200

    refactor

--- src/main/java/org/game/state/State.java
+++ src/main/java/org/game/state/State.java
@@ -3,17 +3,11 @@ package org.game.state;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.game.map.Surface;
-import org.game.unit.GameUnit;
-
-import java.util.Map;
 
 @Getter
 @Setter
 @Builder
 public class State {
-    Surface [] [] map;
-    Map <String, GameUnit> fleet;
     MapAreaState mapAreaState;
     LogAreaState logAreaState;
     InfoAreaState infoAreaState;

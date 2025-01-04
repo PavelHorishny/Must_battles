4b2596c53c2cebde6869763fd4c6a187b9905650
Pavel Horishny <horishnypavel@gmail.com>
2025-01-04 14:21:42 +0200

    GameState added

--- src/main/java/org/game/state/GameState.java
+++ src/main/java/org/game/state/GameState.java
@@ -1,5 +1,31 @@
 package org.game.state;
 
+import lombok.*;
+import org.game.EndGame;
+import org.game.gui.Coordinates;
+import org.game.map.Surface;
+import org.game.unit.GameUnit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameState {
+    private boolean buttonOnRepairActive = false;
+    private boolean buttonReadyForHelpActive = false;
+    private final EndGame endGame = new EndGame();
+    private GameUnit selected = null;
+    private GameUnit target = null;
+    private Coordinates vesselInStorm = null;
+    private Coordinates stormDestination = null;
+    private Surface[][] map;
+    private final ArrayList<Surface> route = new ArrayList<>();
+    private final ArrayList <GameUnit> aimedUnits = new ArrayList<>();
+    private final Map<String, GameUnit> fleet = new HashMap<>();
+    private int day = 0;
+    private boolean isFirstPlayerMove = true;
 }
-//todo may be redundant

f1439a43466378e87c40070ecf2486c5cd0e6344
Pavel Horishny <horishnypavel@gmail.com>
2025-01-05 23:10:01 +0200

    update

--- src/test/java/org/game/services/UnitProcessorTest.java
+++ src/test/java/org/game/services/UnitProcessorTest.java
@@ -3,16 +3,14 @@ package org.game.services;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UnitProcessorTest {
-    static UnitProcessor unitProcessor;
+    static GameProcessor unitProcessor;
     @BeforeAll
     static void start(){
-        unitProcessor = new UnitProcessor();
+        unitProcessor = new GameProcessor();
     }
-    @Test
+/*    @Test
     void prt(){
         unitProcessor.getFleet().forEach((k,v)->assertEquals(k,v.getId()));
-    }
+    }*/
 }

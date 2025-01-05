c80b05c88018303102e9aed1a6b927cc5b702ca1
Pavel Horishny <horishnypavel@gmail.com>
2025-01-05 23:04:35 +0200

    TakingPartAtRepair interface added, isUnitReadyToTakePartAtRepair added

---
+++ src/main/java/org/game/services/TakingPartAtRepair.java
@@ -0,0 +1,8 @@
+package org.game.services;
+
+import org.game.map.Surface;
+import org.game.unit.GameUnit;
+
+public interface TakingPartAtRepair {
+    boolean isUnitReadyToTakePartAtRepair(GameUnit gameUnit, Surface [] [] map);
+}

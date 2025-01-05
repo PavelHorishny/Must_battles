f26e314d2f1da8c4eb76015781a5f1f5d794594a
Pavel Horishny <horishnypavel@gmail.com>
2025-01-05 23:13:00 +0200

    updated

--- src/main/java/org/game/messaging/GameServer.java
+++ src/main/java/org/game/messaging/GameServer.java
@@ -1,12 +1,12 @@
 package org.game.messaging;
 
 import org.game.*;
-import org.game.services.UnitProcessor;
-import org.game.services.UnitService;
+import org.game.services.GameProcessor;
+import org.game.services.GameService;
 
 
 public class GameServer implements Server{
-    private final UnitService unitService = new UnitProcessor();
+    private final GameService gameService = new GameProcessor();
     public GameServer() {
         Context.setServer(this);
     }
@@ -14,14 +14,14 @@ public class GameServer implements Server{
     @Override
     public void onRequest(Client client, Request request) {
         switch (request.getMessage()){
-            case SELECT -> client.response(unitService.unitSelected(request.getId()));
-            case START -> client.response(unitService.initialGameState());
-            case MOVEMENTS_START -> client.response(unitService.movementStarts(request.getId()));
-            case MOVEMENTS_END -> client.response(unitService.movementEnds(request.getId(),request.getDestination()));
-            case SHOT -> client.response(unitService.makeShot(request.getId(),request.getTargetID(),request.getShotType()));
-            case END -> client.response(unitService.dayEnd());
-            case REPAIR -> client.response(unitService.unitReadyForRepair(request.isState()));
-            case HELP -> client.response(unitService.unitReadyForHelp(request.isState()));
+            case SELECT -> client.response(gameService.unitSelected(request.getId()));
+            case START -> client.response(gameService.initialGameState());
+            case MOVEMENTS_START -> client.response(gameService.movementStarts(request.getId()));
+            case MOVEMENTS_END -> client.response(gameService.movementEnds(request.getId(),request.getDestination()));
+            case SHOT -> client.response(gameService.makeShot(request.getId(),request.getTargetID(),request.getShotType()));
+            case END -> client.response(gameService.dayEnd());
+            case REPAIR -> client.response(gameService.unitReadyForRepair(request.isState()));
+            case HELP -> client.response(gameService.unitReadyForHelp(request.isState()));
         }
     }
 }

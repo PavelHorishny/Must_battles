bac06a3087da6066f1ecb052e0c1eec0c4dc9120
Pavel Horishny <horishnypavel@gmail.com>
2025-01-05 23:05:37 +0200

    methods moved to separate interface

--- src/main/java/org/game/services/VesselService.java
+++ src/main/java/org/game/services/VesselService.java
@@ -9,12 +9,14 @@ import java.util.List;
 import java.util.Map;
 
 public interface VesselService {
-    void setTheFleet(Map<String, GameUnit> fleet, Surface [][] map);
+    //void setTheFleet(Map<String, GameUnit> fleet, Surface [][] map);
     List<Vessel> getListOfAllVessels(Map <String, GameUnit> fleet);
-    boolean checkIfVesselCanBeRepaired(Vessel vessel, Surface [] [] map);
+    //boolean checkIfVesselCanBeRepaired(Vessel vessel, Surface [] [] map);
     void moveVesselToDestinationPoint(Vessel vessel, Coordinates destination, Surface[][] map);
     //void restoreVesselsData(Map<String, GameUnit> fleet);
     void checkVesselsAtDayEnd(Map<String, GameUnit> fleet);
-    boolean checkIfVesselCanHelp(Vessel vessel, Surface[][] map);
+    //boolean checkIfVesselCanHelp(Vessel vessel, Surface[][] map);
     void destroyRepairingVessels(GameUnit unit, Map<String, GameUnit> fleet);
+
+    void setAllVessels(Map<String, GameUnit> fleet, Surface[][] map);
 }

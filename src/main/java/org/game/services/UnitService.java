650876b828908780135528e200be3d7fb1e3312e
Pavel Horishny <horishnypavel@gmail.com>
2025-01-06 17:19:29 +0200

    add new methods

--- src/main/java/org/game/services/UnitService.java
+++ src/main/java/org/game/services/UnitService.java
@@ -1,10 +1,17 @@
 package org.game.services;
 
 import org.game.state.GameState;
+import org.game.unit.GameUnit;
 
 public interface UnitService {
     void setAllUnits(GameState gameState);
 
     void setButtonsState(GameState state);
+
+    void onDestruction(GameUnit unit, GameState state);
+
+    void OnTurnEnd(GameState state);
+
+    void OnDayEnd(GameState state);
 
 }

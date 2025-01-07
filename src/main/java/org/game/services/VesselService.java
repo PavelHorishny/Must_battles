22aabb24d495ed5f0dd3e683c3ca137de914aec7
Pavel Horishny <horishnypavel@gmail.com>
2025-01-07 13:20:01 +0200

    refactor

--- src/main/java/org/game/services/VesselService.java
+++ src/main/java/org/game/services/VesselService.java
@@ -1,6 +1,5 @@
 package org.game.services;
 
-import org.game.gui.Coordinates;
 import org.game.map.Surface;
 import org.game.unit.GameUnit;
 import org.game.unit.Vessel;
@@ -8,9 +7,8 @@ import org.game.unit.Vessel;
 import java.util.List;
 import java.util.Map;
 
-public interface VesselService extends Repairable,TakingPartAtRepair,Destroyable {
+public interface VesselService extends Repairable,TakingPartAtRepair,Destroyable,Moveable {
     List<Vessel> getListOfAllVessels(Map <String, GameUnit> fleet);
-    void moveVesselToDestinationPoint(Vessel vessel, Coordinates destination, Surface[][] map);
     void setAllVessels(Map<String, GameUnit> fleet, Surface[][] map);
     void setFortificationService(FortificationService service);
 

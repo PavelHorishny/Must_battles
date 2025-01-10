0962d4ec5adf1b9cfeb8baacb5524985edea44b9
Pavel Horishny <horishnypavel@gmail.com>
2025-01-10 16:30:55 +0200

    refactor

--- src/main/java/org/game/services/FortificationService.java
+++ src/main/java/org/game/services/FortificationService.java
@@ -10,24 +10,14 @@ import java.util.Map;
 
 public interface FortificationService extends Repairable,Destroyable{
     boolean isRoyalPortIsNotEmpty(Fortification fortification);
-
     void setStandardFortifications(Map<String,GameUnit> fleet, Surface [] [] map);
-
     List<Fortification> getFortificationsOfPlayer(Map<String, GameUnit> fleet, boolean isFirstPlayer);
-
     List<Fortification> getFortificationsByType(List<Fortification> forts, FortificationType firstLineFort);
-
     Fortification getFortWIthBigPort(List<Fortification> fortificationsOfPlayer);
     void setVesselService(VesselService service);
-
     List<Fortification> getAllFortifications(Map<String, GameUnit> fleet);
-
-
     String testString(Fortification fortification);
-
     boolean checkIfPortIsRoyal(Fortification fortification);
-
     boolean checkIfCanShoot(Fortification fortification);
-
     void restoreRoyalPort(Fortification fortification);
 }

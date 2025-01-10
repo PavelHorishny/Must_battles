7a74b4a75ea83b162a900ece3486cc097dd92818
Pavel Horishny <horishnypavel@gmail.com>
2025-01-10 16:30:44 +0200

    refactor

--- src/main/java/org/game/services/MapService.java
+++ src/main/java/org/game/services/MapService.java
@@ -15,18 +15,12 @@ public interface MapService {
     void getRandomMap();
     Set<Surface> getPort(Coordinates coordinates, Surface [] [] map);
     void getRoute(GameUnit unit, ArrayList<Surface> route, Surface [][] map);
-
     boolean checkIfPositionIsWater(Surface[][] map, Coordinates c);
-
     void clearRoute(ArrayList<Surface> route);
     List<GameUnit> getFiringZone(GameUnit unit, Surface [] [] map);
     boolean isNotInPort(GameUnit unit,Surface [] [] map);
     boolean checkIfPositionIsPort(Surface[][] map, Coordinates c);
-
     void addUnit(GameUnit gameUnit, Surface[][] map);
-
     void removeUnit(GameUnit gameUnit, Surface[][] map);
-
     int getDistance(Coordinates attacker, Coordinates target);
-    //TODO remake generateStandardMap (Surface [][] map) <- change signature
 }

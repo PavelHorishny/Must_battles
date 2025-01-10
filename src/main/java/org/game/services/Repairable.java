a8c686ecd160b2ced6437d0eaf4ba06fc6ab35be
Pavel Horishny <horishnypavel@gmail.com>
2025-01-10 16:30:30 +0200

    refactor

--- src/main/java/org/game/services/Repairable.java
+++ src/main/java/org/game/services/Repairable.java
@@ -6,8 +6,6 @@ import org.game.unit.GameUnit;
 public interface Repairable {
     boolean isUnitCanBeRepaired(GameUnit gameUnit, Surface [] [] map);
     void repairUnit(GameUnit gameUnit);
-
     void setRepairableStates(boolean state, GameUnit selected);
-
     void setUnitOnRepair(GameUnit unit);
 }

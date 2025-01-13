46e859a3647a291dacb2e7ae5708d62103028134
Pavel Horishny <horishnypavel@gmail.com>
2025-01-13 13:09:39 +0200

    Logger added

--- src/main/java/org/game/messaging/GameClient.java
+++ src/main/java/org/game/messaging/GameClient.java
@@ -1,19 +1,24 @@
 package org.game.messaging;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.game.Request;
 import org.game.state.State;
 import org.game.gui.panels.Mediator;
 
 public class GameClient implements Client{
+    public static final Logger logger = LogManager.getLogger(GameClient.class);
     Mediator mediator;
     Server server;
     @Override
     public void request(Request request) {
+        logger.debug("send request {}", request);
         server.onRequest(this, request);
     }
 
     @Override
     public void connect(Server server) {
+        logger.debug("Connected to {}",server);
         this.server=server;
     }
 
@@ -24,6 +29,7 @@ public class GameClient implements Client{
 
     @Override
     public void response(State state) {
+        logger.debug(state);
         mediator.update(state);
     }
 }

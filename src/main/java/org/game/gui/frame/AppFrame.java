70c2ac6fdc3952dd3aac10b5ea240f40beecbc97
Pavel Horishny <horishnypavel@gmail.com>
2025-01-13 13:08:15 +0200

    Logger added

--- src/main/java/org/game/gui/frame/AppFrame.java
+++ src/main/java/org/game/gui/frame/AppFrame.java
@@ -1,13 +1,17 @@
 package org.game.gui.frame;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.game.gui.panels.AppPanel;
 import javax.swing.*;
 
 public class AppFrame extends JFrame {
+    public static final Logger logger = LogManager.getLogger(AppFrame.class);
     public AppFrame() {
         setVisible(true);
         add(new AppPanel());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
+        logger.debug("AppFrame runs");
     }
 }

6836d364bc6d870e7d29f926eb292533915a1d04
Pavel Horishny <horishnypavel@gmail.com>
2025-01-13 13:08:51 +0200

    Logger added

--- src/main/java/org/game/gui/panels/AppPanel.java
+++ src/main/java/org/game/gui/panels/AppPanel.java
@@ -1,5 +1,7 @@
 package org.game.gui.panels;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.game.gui.panels.game.GamePanel;
 import org.game.gui.panels.main.LoadPanel;
 import org.game.gui.panels.main.MainPanel;
@@ -9,9 +11,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class AppPanel extends JPanel {
+    public static final Logger logger = LogManager.getLogger(AppPanel.class);
     public AppPanel(){
+        logger.trace("AppPanel started");
         setLayout(new CardLayout());
         setPreferredSize(new Dimension(1280,960));
+        //TODO magic number
         add(new MainPanel(),"Main");
         add(new LoadPanel(), "Load");
         add(new SettingsPanel(),"Settings");

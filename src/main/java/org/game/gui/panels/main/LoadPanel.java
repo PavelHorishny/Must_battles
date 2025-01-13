a3852ec3e9d7d16d79200fc0ae0940fb83257f8b
Pavel Horishny <horishnypavel@gmail.com>
2025-01-13 13:12:00 +0200

    Logger added

--- src/main/java/org/game/gui/panels/main/LoadPanel.java
+++ src/main/java/org/game/gui/panels/main/LoadPanel.java
@@ -1,13 +1,18 @@
 package org.game.gui.panels.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class LoadPanel extends JPanel {
+    public static final Logger logger = LogManager.getLogger(LoadPanel.class);
     JButton back = new JButton("Back");
     public LoadPanel (){
         this.add(new Label("Here will be list of saved games"));
         back.addActionListener(e->{
+            logger.info("Back button pressed");
             CardLayout layout = (CardLayout) this.getParent().getLayout();
             layout.show(this.getParent(),"Main");
         });

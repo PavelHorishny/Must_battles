03f2930758027c4eccf50f366fa14180581f9f73
Pavel Horishny <horishnypavel@gmail.com>
2025-01-16 20:58:09 +0200

    added new field for list of log messages

--- src/main/java/org/game/state/LogAreaState.java
+++ src/main/java/org/game/state/LogAreaState.java
@@ -4,9 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Getter
 @Setter
 @Builder
 public class LogAreaState extends GameComponentState {
     private String label;
+    private ArrayList<String> log;
 }

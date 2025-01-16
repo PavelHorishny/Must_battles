02e6fcc004b69bd27dae18f9d25b487ed3b3c71b
Pavel Horishny <horishnypavel@gmail.com>
2025-01-16 21:01:32 +0200

    log service interface is added

---
+++ src/main/java/org/game/services/LogService.java
@@ -0,0 +1,26 @@
+package org.game.services;
+
+import org.game.gui.Coordinates;
+import org.game.gui.panels.Message;
+import org.game.state.GameState;
+import org.game.unit.GameUnit;
+
+import java.util.ArrayList;
+
+public interface LogService {
+    void setShortMessage(GameState state, String message);
+    void setLongMessage(String message);
+
+    void startMessage();
+
+    void selectedMessage(String logMessage);
+
+    void addMessage(/*GameState state,*/ Message message, String text);
+
+    void setLog(GameState state);
+    void setMessage(String message);
+    void setMessages(ArrayList<String>messages);
+    void setState(GameState state);
+
+    void moveMessage(GameUnit gameUnit, Coordinates destination);
+}

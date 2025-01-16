9ef1c366e4df828328ceb8e87096727e8b726d1f
Pavel Horishny <horishnypavel@gmail.com>
2025-01-16 20:57:33 +0200

    mouse listener added to label field and dialog for printing log messages

--- src/main/java/org/game/gui/panels/game/areas/LogArea.java
+++ src/main/java/org/game/gui/panels/game/areas/LogArea.java
@@ -9,16 +9,60 @@ import org.game.gui.panels.Mediator;
 import org.game.gui.panels.Settings;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class LogArea extends GamePanelComponent {
     private static final Logger logger = LogManager.getLogger(LogArea.class);
     LogAreaState state;
     JLabel l;
+    JTextArea log;
 
     public LogArea(Settings settings){
         super(settings);
         l = new JLabel(name);
         add(l);
+        l.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                JDialog d = new JDialog();
+                JButton ok = new JButton("ok");
+                log = new JTextArea();
+                d.add(log);
+                d.add(ok);
+                log.setEnabled(false);
+                d.setSize(600,600);
+                d.setUndecorated(true);
+                d.setLocationRelativeTo(null);
+                d.getContentPane().setBackground(Color.MAGENTA);
+                d.setVisible(true);
+                d.setLayout(new FlowLayout());
+                log.removeAll();
+                state.getLog().forEach(string -> log.append(string+"\n"));
+                ok.addActionListener(actionEvent -> d.dispose());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+
+            }
+        });
         logger.debug("runs");
     }
 
@@ -26,7 +70,7 @@ public class LogArea extends GamePanelComponent {
     public void updateState(GameComponentState state) {
         if(state!=null){
             this.state = (LogAreaState) state;
-            if(!this.state.getLabel().isBlank()) l.setText(this.state.getLabel());
+            if(!this.state.getLabel().isEmpty()) l.setText(this.state.getLabel());
             repaint();
         }
 

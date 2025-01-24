adae91f579cd6889572a4803f1a470e19d17a8ad
Pavel Horishny <horishnypavel@gmail.com>
2025-01-24 20:43:44 +0200

    new method added

--- src/main/java/org/game/services/FiringService.java
+++ src/main/java/org/game/services/FiringService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FiringService {
+    void setLogProcessor(LogService processor);
     void setUnderAttack(List<GameUnit> inFiringZone, ArrayList<GameUnit> aimedUnits, GameUnit attacker);
     Optional<GameUnit> shot(GameUnit attacker, GameUnit target);
     Optional<GameUnit> salvoShot(GameUnit attacker, GameUnit target);
